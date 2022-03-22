69e3d1f5b66c6b640cca7822974444ed076a8aff
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2022-03-22 13:44:40 -0300

    Criando middlewares de autenticação personalizados

--- src/usuarios/index.js
+++ src/usuarios/index.js
@@ -1,6 +1,8 @@
+
 module.exports = {
   rotas: require('./usuarios-rotas'),
   controlador: require('./usuarios-controlador'),
   modelo: require('./usuarios-modelo'),
-  estrategiaAutenticacao: require('./estrategias-autenticacao')
+  estrategiaAutenticacao: require('./estrategias-autenticacao'),
+  middlewaresAutenticacao: require('./middlewares-autenticacao')
 }
--- src/usuarios/usuarios-rotas.js
+++ src/usuarios/usuarios-rotas.js
@@ -1,6 +1,5 @@
 const usuariosControlador = require('./usuarios-controlador');
 const middlewaresAutenticacao = require('./middlewares-autenticacao')
-const passport = require('passport')
 
 module.exports = app => {
   app
@@ -15,7 +14,7 @@ module.exports = app => {
   app
     .route('/usuario/:id')
     .delete(
-      passport.authenticate('bearer', { session: false }),
+      middlewaresAutenticacao.bearer,
       usuariosControlador.deleta
     );
 };
